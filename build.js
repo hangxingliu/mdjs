f3ed95c36d6254290e6f25d75f315918df426d42
LiuYue <hangxingliu@gmail.com>
2017-03-07 14:53:26 +0800

    new build script(add source mao and babel-js )

--- build.js
+++ build.js
@@ -1,16 +1,58 @@
 #!/usr/bin/env node
 
 const TO = 'mdjs.min.js',
+	TO_SOURCE_MAP = 'mdjs.min.js.map',
 	FROM = 'mdjs.js';
 
 var compressor = require('uglify-js'),
-	fs = require('fs');
+	babel = require('babel-core'),
+	{ existsSync, unlinkSync, writeFileSync } = require('fs');
 
-try {
-	fs.existsSync(TO) && fs.unlinkSync(TO);
-	fs.writeFileSync(TO, compressor.minify(FROM).code);
-	console.log('\n  build success!\n');
-} catch (e) {
-	console.error(e.stack);
-	console.log('\n  build failed!\n');	
+(function main() {
+	console.log(`Cleaning target file "${TO}"...`);
+	cleanTargetFile();
+
+	console.log("Babel transforming...");
+	babel.transformFile(FROM, {
+		plugins: [
+			"transform-es2015-template-literals",
+			"transform-es2015-arrow-functions"
+		],
+		sourceMaps: true
+	}, (err, { code, map}) => {
+		err && throwError(err);
+		try {
+			console.log("UglifyJs minifying codes...");
+			var result = compressor.minify(code, {
+				fromString: true,
+				inSourceMap: map,
+				outSourceMap: TO_SOURCE_MAP
+			});
+
+			console.log("Writing to target file...");
+			writeFileSync(TO, result.code);
+			writeFileSync(TO_SOURCE_MAP, result.map );
+			
+			console.log('\n  build success!\n');
+
+		} catch (e) {
+			throwError(e);
+		}
+	});
+
+	
+} )();
+
+function cleanTargetFile() {
+	try {
+		existsSync(TO) && unlinkSync(TO);
+	} catch (e) {
+		throwError(e);
+	}
+}
+
+function throwError(error) {
+	console.error(error.stack || error);
+	console.error('\n  build failed!\n');
+	process.exit(1);
 }
