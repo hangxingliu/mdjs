5eb5978a53def3d3c048cafcc007655bb62ce483
Liu Yue <hangxingliu@gmail.com>
2021-11-08 12:00:13 +0800

    update GFM test cases

--- v2/src/test/main/gfm/ThematicBreaks.ts
+++ v2/src/test/main/gfm/ThematicBreaks.ts
@@ -4,16 +4,11 @@ import { deepStrictEqual } from "assert";
 import { md2html } from "../../../main";
 
 export async function main(logger: Logger) {
-  logger.log(`Example 13`);
-  deepStrictEqual(md2html(`***\n---\n___`), "<hr /><hr /><hr />");
-
-  logger.log(`Example 14`);
-  deepStrictEqual(md2html(`+++`), "<p>+++</p>");
-
-  logger.log(`Example 15`);
-  deepStrictEqual(md2html(`===`, { gfm: true }), "<p>===</p>");
-
-  logger.log(`Example 16`);
-  // <p>--\n**\n__</p>
-  deepStrictEqual(md2html(`--\n**\n__`, { gfm: true }), "<p>--**__</p>");
+  const cases = require('./ThematicBreaks.json');
+  for (let i = 0; i < cases.length; i++) {
+    const { id, comment, input, output } = cases[i];
+    const testName = id + (comment ? ` "${comment}"` : '');
+    logger.log(testName);
+    deepStrictEqual(md2html(input, { gfm: true }), output, testName);
+  }
 }
