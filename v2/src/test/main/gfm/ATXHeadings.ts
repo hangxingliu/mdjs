13ab2a359f098057c16ef1753da84b2caa4052d8
Liu Yue <hangxingliu@gmail.com>
2021-11-11 14:14:00 +0800

    add tests for ATX heading

---
+++ v2/src/test/main/gfm/ATXHeadings.ts
@@ -0,0 +1,14 @@
+import { Logger } from "../../base";
+import { deepStrictEqual } from "assert";
+
+import { md2html } from "../../../main";
+
+export async function main(logger: Logger) {
+  const cases = require('./ATXHeadings.json');
+  for (let i = 0; i < cases.length; i++) {
+    const { id, comment, input, output } = cases[i];
+    const testName = id + (comment ? ` "${comment}"` : '');
+    logger.log(testName);
+    deepStrictEqual(md2html(input, { gfm: true, headingId: false }), output, testName);
+  }
+}
